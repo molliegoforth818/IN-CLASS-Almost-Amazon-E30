c2cc2f455667b7ce734be99d994738c3d56da2d4
Mollie Goforth <molliegoforth818@gmail.com>
2025-03-04 20:47:00 -0600

    read, filter, delete on authors complete

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,8 +1,8 @@
 import { signOut } from '../utils/auth';
 import { getBooks, booksOnSale } from '../api/bookData';
 import { showBooks } from '../pages/books';
-// import { getAuthors } from '../api/authorData';
-// import { showAuthors } from '../pages/authors';
+import { favAuthors, getAuthors } from '../api/authorData';
+import { showAuthors } from '../pages/authors';
 
 // navigation events
 const navigationEvents = () => {
@@ -24,9 +24,11 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
-    // getAuthors().then(showAuthors);
+    getAuthors().then(showAuthors);
   });
-
+  document.querySelector('#fav-authors').addEventListener('click', () => {
+    favAuthors().then(showAuthors);
+  });
   // STRETCH: SEARCH
   document.querySelector('#search').addEventListener('keyup', (e) => {
     const searchValue = document.querySelector('#search').value.toLowerCase();
